9fb81444421113d1d489b4251f273afe49d7c98b
sepanta <sp889jk@gmail.com>
2026-02-25 12:31:32 +0330

    added cart admin

--- cart/admin.py
+++ cart/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from .models import Cart, CartItem
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    autocomplete_fields = ["product", "variant"]
+
+    def subtotal_display(self, obj):
+        return obj.subtotal
+
+    subtotal_display.short_description = "Subtotal"
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["user", "session_key", "is_active"]
+    search_fields = ["session_key", "user"]
+    list_filter = ["is_active"]
+    empty_value_display = "-empty-"
+    inlines = [CartItemInline]
